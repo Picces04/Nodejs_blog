314d566a25c86d790922fc0e25c08eafac26e410
Picces04 <khanhhaivodoi@gmail.com>
2025-05-10 16:45:33 +0700

    Create Base Express + Thư viện ( Nodemon, Morgan, Template Engine, Scss, Bootstrap4 )

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resource/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
