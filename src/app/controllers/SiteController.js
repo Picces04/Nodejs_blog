cf39ce8870c4c5c6168fde87da64dba565995acb
Picces04 <khanhhaivodoi@gmail.com>
2025-05-23 23:57:27 +0700

    Connect MongoDB Atlas

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,15 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /Site
-    index(req, res) {
-        res.render('home');
+    async index(req, res) {
+        try {
+            const courses = await Course.find({});
+            console.log('Courses:', courses); // Log the query result
+            res.json(courses); // Send the courses as a JSON response
+        } catch (err) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
 
     search(req, res) {
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/node-f8-dev');
+        await mongoose.connect(
+            'mongodb+srv://Nguyenphuhai143:Hai14032004@cluster0.rajwy0r.mongodb.net/node-f8-dev?retryWrites=true&w=majority&appName=Cluster0',
+        );
         console.log('Connect successfully');
+        console.log('Cơ sở dữ liệu:', mongoose.connection.name); // Xác nhận database
     } catch (error) {
         console.log('Connect failed !!!', error);
     }
